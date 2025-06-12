3aaa060f7da1f1f9510b94d3f8408cae13f1309e
Yurii <yerikstar@gmail.com>
2025-06-12 16:54:35 +0300

    add task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
-function slugify(title) {
-    const titleLowerCase = title.toLowerCase();
-    const words = titleLowerCase.split(' ');
-    const args = Array.from(words);
-    return args.join('-');
-}
-console.log(slugify('Arrays for beginners'));
-console.log(slugify('English for developer'));
-console.log(slugify('Ten secrets of JavaScript'));
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+// function slugify(title) {
+//     const titleLowerCase = title.toLowerCase();
+//     const words = titleLowerCase.split(' ');
+//     const args = Array.from(words);
+//     return args.join('-');
+// }
+// console.log(slugify('Arrays for beginners'));
+// console.log(slugify('English for developer'));
+// console.log(slugify('Ten secrets of JavaScript'));
+// console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
--- js/task-2.js
+++ js/task-2.js
@@ -1,42 +1,14 @@
-// function formatMessage(message, maxLength) {
-//     if (message.length <= maxLength) {
-//         return `${message}`;
-//     }
-//     else {
-//         return message.slice(0, maxLength) + '...';
-//     }
-// }
-// console.log(formatMessage("Curabitur ligula sapien", 16));
-// console.log(formatMessage("Curabitur ligula sapien", 23));
-// console.log(formatMessage("Vestibulum facilisis purus nec", 20));
-// console.log(formatMessage("Vestibulum facilisis purus nec", 30));
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
-
-
-
-
-
-
-
-
-// function foo() {
-    // У змінній args буде повноцінний масив з усіх аргументів
-//     const args = Array.from(arguments);
-//       return args.join("-");
-//   }
-  
-//   foo(1, 2, 3); // Поверне "1-2-3"
-
-
-// const name = "Mango";
-// const letters = name.split("");
-// console.log(letters); // ["M", "a", "n", "g", "o"]
-
-// const message = "JavaScript essentials";
-// const words = message.split(" ");
-// console.log(words); // ["JavaScript", "essentials"]
-
-// const slug = "amazing-french-recipes";
-// const slugParts = slug.split("-");
-// console.log(slugParts); // ["amazing", "french", "recipes"]
+function makeArray(firstArray, secondArray, maxLength) {
+    const allArray = firstArray.concat(secondArray);
+    if (allArray.length >= maxLength) {
+        return allArray.slice(0, maxLength);
+    } else {
+        return allArray.slice();
+    }
+}
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));

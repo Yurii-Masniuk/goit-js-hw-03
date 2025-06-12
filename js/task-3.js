f6e39b1d9dc197d2dc349eb71f6856af1f35efa3
Yurii <yerikstar@gmail.com>
2025-06-12 20:36:33 +0300

    add task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,14 @@
-// function checkForSpam(message) {
-//     const contMessage = message.toLowerCase();
-//     return contMessage.includes('spam') || contMessage.includes('sale');
-// }
-// console.log(checkForSpam("Latest technology news"));
-// console.log(checkForSpam("JavaScript weekly newsletter"));
-// console.log(checkForSpam("Get best sale offers now!"));
-// console.log(checkForSpam("Amazing SalE, only tonight!"));
-// console.log(checkForSpam("Trust me, this is not a spam message"));
-// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-// console.log(checkForSpam("[SPAM] How to earn fast money?"));
+function filterArray(numbers, value) {
+    let arrayNew = [];
+    for (let element of numbers) {
+        if (element > value) {
+            arrayNew.push(element);
+        }
+    }
+    return arrayNew;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));

178d0244e0adf21bc7385f39357a49cff9791b02
Yurii <yerikstar@gmail.com>
2025-06-12 16:29:44 +0300

    add task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,10 @@
-// function makeTransaction(quantity, pricePerDroid, customerCredits) {
-//     const totalPrice = quantity * pricePerDroid;
-//     if (totalPrice > customerCredits) {
-//         return 'Insufficient funds!';
-//     }
-//     else {
-//         return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-//     }
-// }
-// console.log(makeTransaction(5, 3000, 23000));
-// console.log(makeTransaction(3, 1000, 15000));
-// console.log(makeTransaction(10, 5000, 8000));
-// console.log(makeTransaction(8, 2000, 10000));
-// console.log(makeTransaction(10, 500, 5000));
-
+function slugify(title) {
+    const titleLowerCase = title.toLowerCase();
+    const words = titleLowerCase.split(' ');
+    const args = Array.from(words);
+    return args.join('-');
+}
+console.log(slugify('Arrays for beginners'));
+console.log(slugify('English for developer'));
+console.log(slugify('Ten secrets of JavaScript'));
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
--- js/task-2.js
+++ js/task-2.js
@@ -12,3 +12,31 @@
 // console.log(formatMessage("Vestibulum facilisis purus nec", 30));
 // console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
 // console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
+
+
+
+
+
+
+
+
+// function foo() {
+    // У змінній args буде повноцінний масив з усіх аргументів
+//     const args = Array.from(arguments);
+//       return args.join("-");
+//   }
+  
+//   foo(1, 2, 3); // Поверне "1-2-3"
+
+
+// const name = "Mango";
+// const letters = name.split("");
+// console.log(letters); // ["M", "a", "n", "g", "o"]
+
+// const message = "JavaScript essentials";
+// const words = message.split(" ");
+// console.log(words); // ["JavaScript", "essentials"]
+
+// const slug = "amazing-french-recipes";
+// const slugParts = slug.split("-");
+// console.log(slugParts); // ["amazing", "french", "recipes"]
